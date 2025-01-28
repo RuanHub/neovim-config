85b1bc5f4ee506528e3c8e6e1b081878ecb49384
RuanHub <ruanvanvuuren9@gmail.com>
2025-01-28 22:48:20 +0400

    Disable line wrap

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -10,6 +10,9 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
 
+-- Stop text from wrapping
+vim.opt.wrap = false
+
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
 
