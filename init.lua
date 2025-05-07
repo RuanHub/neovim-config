1f1a04a2b7fda8298b14b0c07e86e59e5f818564
RuanHub <ruanvanvuuren9@gmail.com>
2025-05-07 09:02:03 +0400

    Add noice to have a nice message display

--- init.lua
+++ init.lua
@@ -53,6 +53,7 @@ require('lazy').setup({
   require 'custom.plugins.toggleterm', -- Plugin to show location of events in scrollbar.
   require 'custom.plugins.diffview', -- Plugin to view diffs
   require 'custom.plugins.octo', -- Plugin to interface with github
+  require 'custom.plugins.noice', -- Plugin to display commands and notices prettier
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
